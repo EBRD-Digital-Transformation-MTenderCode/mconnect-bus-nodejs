703fe358863ff89fabc3f3943333ff920f28d45f
volkov <mr.rotberry@gmail.com>
2019-06-20 02:27:17 +0300

    add node kafka

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,12 @@ import express from 'express';
 import { IMain, IDatabase } from 'pg-promise';
 import pgPromise from 'pg-promise';
 
+import * as kafka from 'kafka-node';
+import { Message } from 'kafka-node';
+
 interface IExtensions {
   find(id: string): Promise<any>;
+
   one: any
 }
 
@@ -23,9 +27,41 @@ const connectUrl: string = 'postgres://postgres:agent@192.168.1.144:5432/postgre
 
 const db = <IDatabase<IExtensions> & IExtensions>pgp(connectUrl);
 
+const kafkaClient = kafka.KafkaClient;
+const kafkaConsumer = kafka.Consumer;
+
+const client = new kafkaClient({
+  kafkaHost: '10.0.20.107:9092,10.0.20.108:9092,10.0.20.109:9092',
+  connectTimeout: 3000,
+  requestTimeout: 3000
+});
+
+client.on("connect", () => {
+  console.log("---> Connected to Kafka");
+});
+
+client.on("ready", () => {
+  console.log("---> Ready listen from Kafka");
+});
+
+const consumer = new kafkaConsumer(client, [{
+  topic: 'transport-agent-in',
+}], {
+  groupId: 'transport-agent-consumer-group',
+});
+
+consumer.on('message', function(message: Message) {
+  console.log('SUCCESSFUL !!! \n');
+  console.log(message);
+});
+
+consumer.on('error', function(err: string) {
+  console.log('ERROR !!!', err);
+});
+
 app.get('/', async function(req, res) {
   try {
-    const response = await db.find("0d20d00c-69c6-13e8-adc0-fa7ae02cce50");
+    const response = await db.find('0d20d00c-69c6-13e8-adc0-fa7ae02cce50');
     res.send(response);
   } catch (e) {
     res.send(JSON.stringify(e));
@@ -33,5 +69,5 @@ app.get('/', async function(req, res) {
 });
 
 app.listen(port, function() {
-  console.log(`Example listening on port - ${port}`);
+  console.log(`Node JS listening on port - ${port}`);
 });
