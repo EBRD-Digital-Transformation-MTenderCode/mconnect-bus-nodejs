cfad81b8aeb7b928f8d44d6017af1a49b3105148
volkov <mr.rotberry@gmail.com>
2019-06-19 19:02:40 +0300

    add connect to db

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
 import express from 'express';
 
+import { IMain, IDatabase } from 'pg-promise';
+import pgPromise from 'pg-promise';
+
+interface IExtensions {
+  find(id: string): Promise<any>;
+  one: any
+}
+
 const port = process.env.PORT || 5000;
 const app: express.Application = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello world from Node JS!!!');
+const pgp: IMain = pgPromise({
+  extend: (obj: IExtensions) => {
+    obj.find = id => {
+      return obj.one('SELECT * FROM public.input WHERE id = $1', id);
+    };
+  },
+});
+
+const connectUrl: string = 'postgres://postgres:agent@192.168.1.144:5432/postgres';
+
+const db = <IDatabase<IExtensions> & IExtensions>pgp(connectUrl);
+
+app.get('/', async function(req, res) {
+  try {
+    const response = await db.find("0d20d00c-69c6-13e8-adc0-fa7ae02cce50");
+    res.send(response);
+  } catch (e) {
+    res.send(JSON.stringify(e));
+  }
 });
 
 app.listen(port, function() {
