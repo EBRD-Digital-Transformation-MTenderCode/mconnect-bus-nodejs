3d5424a280f26a42e48750f049e239a0d9f29795
volkov <mr.rotberry@gmail.com>
2019-06-20 22:31:12 +0300

    add module db

---
+++ src/modules/db/controllers.ts
@@ -0,0 +1,25 @@
+import db from '../db';
+
+import { IIn } from '../../types';
+
+
+import { Message as IMessage } from 'kafka-node';
+
+import { contractRegister } from "../../api";
+
+export const saveIn = async (message: IMessage) => {
+  console.log('--> Message received');
+
+  const { offset, value } = message;
+
+  try {
+    const messageData: IIn = JSON.parse(`${value}`);
+
+    // await db.one(`INSERT INTO public.in VALUES($1, $2) RETURNING ocid`, [messageData.context.ocid, value]);
+
+    contractRegister(messageData);
+
+  } catch (e) {
+    console.log(e.message);
+  }
+};
