684c36319adfea86dd203cff58e047e83ac8b761
volkov <mr.rotberry@gmail.com>
2019-07-02 13:15:07 +0300

    add redeploy scripts

---
+++ redeploy-scripts/dev.sh
@@ -0,0 +1,25 @@
+echo 'Re-deploying'
+docker service create --name="mconnect-bus-nodejs" \
+  --env SERVICE_PORT=5000 \
+  --env KAFKA_HOST=10.0.20.107:9092,10.0.20.108:9092,10.0.20.109:9092 \
+  --env IN_TOPIC=mconnect-bus-in \
+  --env IN_GROUP_ID=mconnect-bus-consumer-group \
+  --env OUT_TOPIC=mconnect-bus-out \
+  --env DB_HOST=10.0.20.112 \
+  --env DB_PORT=5432 \
+  --env DB_NAME=mconnect_bus_db \
+  --env DB_USER=postgres \
+  --env DB_PASSWORD=postgres \
+  --env DB_TABLE_REQUESTS=public.requests \
+  --env DB_TABLE_RESPONSES=public.responses \
+  --env DB_TABLE_TREASURY_REQUESTS=public.treasury_requests \
+  --env DB_TABLE_TREASURY_RESPONSES=public.treasury_responses \
+  --env PP_BASE_URL=http://10.0.20.126:9111 \
+  --env TREASURY_BASE_URL=http://10.47.14.5 \
+  --env LOG_FILE_SIZE_MB=5 \
+  --env LOG_FILES_SAVE_DAYS=30 \
+  --publish published=5000,target=5000 \
+  --network ocds-network \
+  --dns 10.0.20.115 \
+  --with-registry-auth \
+  dev.docker-registry.eprocurement.systems/mconnect-bus:1.1.1.X
