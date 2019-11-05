287d6b84d748ffec083d2ad2b328e110e3b145d4
Dmytro Vasylkivskyi <drizzer14@gmail.com>
2019-11-05 15:13:37 +0200

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+	'^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '.*\\.spec.(ts|tsx|js)$',
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coverageThreshold: {
+	global: {
+	  branches: 80,
+	  functions: 80,
+	  lines: 80,
+	  statements: -250
+	}
+  },
+  collectCoverageFrom: [
+	'src/modules/**/*.{ts,tsx}'
+  ],
+  clearMocks: true,
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['node_modules', 'src']
+};
