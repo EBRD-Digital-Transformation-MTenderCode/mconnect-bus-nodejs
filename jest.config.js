2ff25cfb81da0876488eae861a2f27db16e6df1c
Dmytro Vasylkivskyi <drizzer14@gmail.com>
2019-11-08 15:17:04 +0200

    Add eslint, prettier, lint-staged, update configs

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,23 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   transform: {
-	'^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '.*\\.spec.(ts|tsx|js)$',
   coveragePathIgnorePatterns: ['/node_modules/'],
   coverageThreshold: {
-	global: {
-	  branches: 80,
-	  functions: 80,
-	  lines: 80,
-	  statements: -250
-	}
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -250
+    }
   },
-  collectCoverageFrom: [
-	'src/modules/**/*.{ts,tsx}'
-  ],
+  collectCoverageFrom: ['src/modules/**/*.{ts,tsx}'],
   clearMocks: true,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^/(.*)$': '<rootDir>/src/$1'
+  }
 };
