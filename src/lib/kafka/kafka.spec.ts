0b7cbb2b093d361a99ec29b7201fe25c0aa3cd1e
Dmytro Vasylkivskyi <drizzer14@gmail.com>
2019-11-25 17:52:57 +0200

    Refactor kafka tests

--- src/lib/kafka/kafka.spec.ts
+++ src/lib/kafka/kafka.spec.ts
@@ -1,13 +1,13 @@
 import { kafkaClientConfig, kafkaInConsumerConfig, kafkaOutProducerConfig } from 'configs';
-import Kafka, { ConsumerGroup, HighLevelProducer, KafkaClient, Offset } from 'kafka-node';
+import { Offset } from 'kafka-node';
+import { OutProducer as Producer } from 'lib/kafka/producer';
+import { InConsumer as Consumer } from 'lib/kafka/consumer';
+import { Client } from 'lib/kafka/client';
 import logger from 'lib/logger';
 
 jest.mock('lib/logger');
 
 describe('[Integration] Kafka', () => {
-  let Client: KafkaClient;
-  let Producer: HighLevelProducer;
-  let Consumer: ConsumerGroup;
   const ready: string[] = [];
 
   const getCurrentOffset = async (topic: string): Promise<number> => {
@@ -32,41 +32,19 @@ describe('[Integration] Kafka', () => {
     In = 'mconnect-bus-in'
   }
 
-  beforeEach(async () => {
-    Consumer = new Kafka.ConsumerGroup(
-      {
-        kafkaHost: kafkaClientConfig.kafkaHost,
-        groupId: kafkaInConsumerConfig.inGroupId,
-        protocol: ['roundrobin'],
-        connectOnReady: true
-      },
-      kafkaInConsumerConfig.inTopic
-    );
-
+  it('Should connect', async () => {
     Consumer.on('connect', () => {
-      logger.info('✔ Kafka Consumer connected');
       ready.push('consumer');
     });
-    Consumer.on('error', error => logger.error('🗙 Error kafka consumer: ', error));
-
-    Client = new Kafka.KafkaClient(kafkaClientConfig);
 
     Client.on('ready', () => {
-      logger.info('✔ Kafka Client ready');
       ready.push('client');
     });
-    Client.on('error', error => logger.error('🗙 Error kafka client: ', error));
-
-    Producer = new Kafka.HighLevelProducer(Client);
 
     Producer.on('ready', () => {
-      logger.info('✔ Kafka Producer ready');
       ready.push('producer');
     });
-    Producer.on('error', error => logger.error('🗙 Error kafka producer: ', error));
-  });
 
-  it('Should connect', async () => {
     await requestTimeout(25000);
 
     expect(ready).toContain('client');
